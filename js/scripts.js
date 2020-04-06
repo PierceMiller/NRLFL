2607f91e52c21eb869f2817beafaa4eec6bc631b
Pierce <piercemiller23@gmail.com>
2020-04-06 21:04:15 +1200

    final design

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
         prevArrow: $('.prev'),
         responsive: [
             {
-              breakpoint: 1024,
+              breakpoint: 1200,
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
@@ -21,14 +21,14 @@ $(document).ready(function() {
               }
             },
             {
-              breakpoint: 900,
+              breakpoint: 1110,
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
               }
             },
             {
-              breakpoint: 600,
+              breakpoint: 720,
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
@@ -36,4 +36,19 @@ $(document).ready(function() {
             }
           ]
       });
+});
+
+ClassicEditor
+.create( document.querySelector( '#body' ), {
+    toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+    heading: {
+        options: [
+            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+        ]
+    }
+} )
+.catch( error => {
+    console.log( error );
 });
